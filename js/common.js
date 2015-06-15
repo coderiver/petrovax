fbd1e621cfa4867b0d537b1b66fded44dcd68ff1
Валерий <kaktuz34@gmail.com>
2015-06-15 15:49:20 +0300

    responsibility

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	// click
 	$('body').on('click', function() {
 		$(this).removeClass('is-open-menu');
+		$('.drop').removeClass('is-open');
 	});
 
 	// menu
@@ -16,5 +17,25 @@ $(document).ready(function() {
 			body.addClass('is-open-menu');
 		}
 	});
+
+	function lMenu() {
+		var menu = $('.l_menu'),
+			active = $('.l_menu').find('a.ac').text();
+
+		if ($(window).width() < 960) {
+			menu.wrapAll("<div class='drop'></div>");
+			$('.drop').append('<div class="drop__text">'+' '+active+' '+' </div>');
+		}
+		$('.drop__text').on('click', function(event) {
+			event.stopPropagation();
+			$('.drop').addClass('is-open');
+		});
+	}
+	lMenu();
 	
+	$('.l_menu a').on('click', function(){
+		var text = $(this).text();
+		$('.drop__text').text(text);
+	});
+
 });
